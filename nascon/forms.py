6916c56ab1b0314a07784938053a56da3c0f365e
RafayAhmad7548 <rafayahmad1425@gmail.com>
2025-05-01 11:46:45 +0500

    homepage fix and remove accomdoatin form signup

--- nascon/forms.py
+++ nascon/forms.py
@@ -5,12 +5,11 @@ class SignupForm(forms.ModelForm):
     
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'role', 'accommodation_id', 'username', 'email', 'password']
+        fields = ['first_name', 'last_name', 'role', 'username', 'email', 'password']
         widgets = {
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
             'role': forms.Select(attrs={'class': 'form-control', 'required': 'false'}),
-            'accommodation_id': forms.Select(attrs={'class': 'form-control'}),
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'password': forms.PasswordInput(attrs={'class': 'form-control'}),
